79209afcc35d53e9232367782b91b18d4844581e
Bohdan Butsev <44702726+winnerok7@users.noreply.github.com>
2020-05-25 08:10:42 +0300

    Add files via upload

---
+++ Price Menu/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Pricing from "./Pricing";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Pricing />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
