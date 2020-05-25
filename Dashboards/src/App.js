96c76ff98d5e8b7d0c895a7172c05031358e9dcb
Bohdan Butsev <44702726+winnerok7@users.noreply.github.com>
2020-05-25 08:08:34 +0300

    Add files via upload

---
+++ Dashboards/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import Dashboard from "./Dashboard";
+import Chart from "./Chart";
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Dashboard />
+        <Chart />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
