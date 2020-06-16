2421ab01e77f32e7d937064b71e7b3191d40e5b3
Bohdan Butsev <44702726+winnerok7@users.noreply.github.com>
2020-06-16 14:40:32 +0300

    Update App.js

--- Blog/src/App.js
+++ Blog/src/App.js
@@ -5,21 +5,10 @@ import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
 import Blog from "./Blog";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit">Your Website</Link> {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-        <Copyright />
         <Blog />
       </Box>
     </Container>
